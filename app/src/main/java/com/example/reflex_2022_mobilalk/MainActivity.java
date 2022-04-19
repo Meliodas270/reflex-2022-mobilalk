08f7f42074975791b86004cc376c21ba76c93e68
Kosik Roland Csaba <h047231@stud.u-szeged.hu>
2022-04-19 13:02:49 +0200

    Created project

---
+++ app/src/main/java/com/example/reflex_2022_mobilalk/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.reflex_2022_mobilalk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
