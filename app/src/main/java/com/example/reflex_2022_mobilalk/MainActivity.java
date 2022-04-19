450e270a850da23f364edd842bd937244bc8808f
Kosik Roland Csaba <h047231@stud.u-szeged.hu>
2022-04-19 14:45:53 +0200

    Updated starting page

--- app/src/main/java/com/example/reflex_2022_mobilalk/MainActivity.java
+++ app/src/main/java/com/example/reflex_2022_mobilalk/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.reflex_2022_mobilalk;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onPlayButtonPushed(View view) {
+    }
+
+    public void onLoginButtonPushed(View view) {
+    }
+
+    public void onRegisterButtonPushed(View view) {
     }
 }
